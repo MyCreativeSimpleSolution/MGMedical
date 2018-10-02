73cc7653f4495e715adc73369d8fff32a3febb20
T04435 <fidel@yabonza.com>
2018-10-03 00:06:38 +1000

    feat(sidebar): Added sidebar
    Added the sidebar, next header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/dashboard/card/card.component';
+import {HttpClientModule} from '@angular/common/http';
+import {InlineSVGModule} from 'ng-inline-svg';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    InlineSVGModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
